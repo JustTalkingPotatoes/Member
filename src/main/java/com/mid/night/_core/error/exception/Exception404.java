c18a243c0e91ff5ab66c406b8a0656cc9ee98dc7
INUK-ai <arheon2469@gmail.com>
2024-08-19 15:00:25 +0900

    Feat: ErrorHandling

---
+++ src/main/java/com/mid/night/_core/error/exception/Exception404.java
@@ -0,0 +1,23 @@
+package com.mid.night._core.error.exception;
+
+import com.mid.night._core.utils.ApiUtils;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+// 잘못된 경로
+@Getter
+public class Exception404 extends RuntimeException {
+    public Exception404(String message) {
+        super(message);
+    }
+
+    public ApiUtils.ApiResult<?> body(){
+        return ApiUtils.error(getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    public HttpStatus status(){
+        return HttpStatus.NOT_FOUND;
+    }
+}
+
+
