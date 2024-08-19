211c88e99e35723434eb79c7f2663889a474057f
INUK-ai <arheon2469@gmail.com>
2024-08-19 15:13:12 +0900

    Feat: SecurityUtils

--- src/main/java/com/mid/night/_core/utils/SecurityUtils.java
+++ src/main/java/com/mid/night/_core/utils/SecurityUtils.java
@@ -1,4 +1,18 @@
 package com.mid.night._core.utils;
 
+import com.mid.night._core.error.exception.Exception403;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class SecurityUtils {
+
+    public static Long getCurrentMemberId() {
+
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        if(name.equals("anonymousUser")) {
+            throw new Exception403("익명의 유저는 접근 권한이 없습니다.");
+        }
+
+        return Long.parseLong(name);
+    }
 }
