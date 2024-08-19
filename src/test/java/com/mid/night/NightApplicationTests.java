62750704471c297d872dce3c6a8255f9a4f4f0e2
INUK-ai <arheon2469@gmail.com>
2024-08-19 14:33:01 +0900

    Setting: 기본 세팅

---
+++ src/test/java/com/mid/night/NightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mid.night;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
