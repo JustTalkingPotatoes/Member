d6bb4ee927c824b0e1ca36017ab8fc7bc7e92183
INUK-ai <arheon2469@gmail.com>
2024-08-19 14:48:41 +0900

    Feat: RefreshToken

--- src/main/java/com/mid/night/member/service/MemberService.java
+++ src/main/java/com/mid/night/member/service/MemberService.java
@@ -1,4 +1,18 @@
 package com.mid.night.member.service;
 
+import com.mid.night.member.repository.MemberRepository;
+import com.mid.night.redis.repository.RefreshTokenRedisRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
 }
--- src/main/java/com/mid/night/redis/repository/RefreshTokenRedisRepository.java
+++ src/main/java/com/mid/night/redis/repository/RefreshTokenRedisRepository.java
@@ -1,4 +1,9 @@
 package com.mid.night.redis.repository;
 
-public interface RefreshTokenRedisRepository {
+import com.mid.night.redis.domain.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, Long> {
+
+    RefreshToken findByRefreshToken(String refreshToken);
 }
