aa3a94ed7d3dbdaa5a30b31b5a0738c4f7fbc28a
INUK-ai <arheon2469@gmail.com>
2024-08-19 15:15:17 +0900

    Feat: CustomUserDetail

--- src/main/java/com/mid/night/member/repository/MemberRepository.java
+++ src/main/java/com/mid/night/member/repository/MemberRepository.java
@@ -1,9 +1,14 @@
 package com.mid.night.member.repository;
 
+import aj.org.objectweb.asm.commons.Remapper;
 import com.mid.night.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
